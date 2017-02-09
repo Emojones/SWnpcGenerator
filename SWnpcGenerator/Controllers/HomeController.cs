6679b9d1b4808df7772276145b57a91458ec1aa0
Michael <emonomics@gmail.com>
2017-02-09 16:36:07 -0500

    crud scaffolded, random name generator built

--- SWnpcGenerator/Controllers/HomeController.cs
+++ SWnpcGenerator/Controllers/HomeController.cs
@@ -1,43 +1,122 @@
-﻿using SWnpcGenerator.Models;
-using System;
+﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Data.Entity;
 using System.Linq;
+using System.Net;
+using System.Web;
 using System.Web.Mvc;
+using SWnpcGenerator.Models;
 
 namespace SWnpcGenerator.Controllers
 {
     public class HomeController : Controller
     {
-        Context context = new Context();
+       private Context db = new Context();
         
         public ActionResult Index()
-        {
-           
-           return View();
- 
+        {           
+           return View(); 
         }
 
-        public ActionResult RandomNPC()
+        public ActionResult Create()
         {
-
            return View();
         }
+
+        // POST: Species/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,SpeciesName,WoundThreshold,StrainThreshold,StartExp,Brawn,Agility,Intellect,Cunning,Willpower,Presence")] Species species)
+        {
+            if (ModelState.IsValid)
+            {
+                //change this to the new DB that is made up of random stuffs
+                db.Spp.Add(species);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(species);
+        }
+
 
         public ActionResult SavedNPC()
-        {
-            var test = context.Spp.Include(c => c.SpeciesName);
-            //this is just for testing until the table for saved npcs is actually created
-            return View(test);
+        {            
+            return View(db.Spp.ToList());
         }
 
-        //TODO ActionResult Add
-        //TODO ActionResult HttpPost Add
 
-        //TODO ActionResult Edit
-        //TODO ActionResult HttpPost Edit
+       
+        // GET: Species/Edit/
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            //TODO update to randomized db
+            Species species = db.Spp.Find(id);
+            if (species == null)
+            {
+                return HttpNotFound();
+            }
+            return View(species);
+        }
 
-        //TODO ActionResult Delete
-        //TODO ActionResult HttpPost Delete
+        // POST: Species/Edit/
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,SpeciesName,WoundThreshold,StrainThreshold,StartExp,Brawn,Agility,Intellect,Cunning,Willpower,Presence")] Species species)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(species).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(species);
+        }
+
+        // GET: Species/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Species species = db.Spp.Find(id);
+            if (species == null)
+            {
+                return HttpNotFound();
+            }
+            return View(species);
+        }
+
+        // POST: Species/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+
+            //TODO: change to randomized db
+            Species species = db.Spp.Find(id);
+            db.Spp.Remove(species);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
     }
 }
--- SWnpcGenerator/Models/RandomName.cs
+++ SWnpcGenerator/Models/RandomName.cs
@@ -1,7 +1,51 @@
-﻿namespace SWnpcGenerator
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SWnpcGenerator
 {
     public class RandomName
     {
-        //TODO: add random name generation script
+        private static Random _random = new Random();
+
+        private static List<string> _firstSyllable = new List<string>
+        {
+            "Hur", "Rylo", "Ka", "Sul", "Hu", "Il", "Dr", "Bo", "Du", "Gr", "Mon"
+        };
+        private static List<string> _secondSyllable = new List<string>
+        {
+            "ma", "oi", "lu", "tha", "Cal", "ro", "", "'", "hori",
+        };
+        private static List<string> _thirdSyllable = new List<string>
+        {
+            "Killian", "Thoris", "Syllus", "Bortan", "Skywalker", "Bodi"
+        };
+
+
+        public static string RandomFirstSyllable()
+        {
+            return _firstSyllable[_random.Next(0, _firstSyllable.Count)];
+        }
+
+        public static string RandomSecondSyllable()
+        {
+            return _secondSyllable[_random.Next(0, _firstSyllable.Count)];
+        }
+
+        public static string RandomThirdSyllable()
+        {
+            return _thirdSyllable[_random.Next(0, _firstSyllable.Count)];
+        }
+
+       
+        string secondPart = " " + _thirdSyllable;
+        public string Generate()
+        {
+            RandomFirstSyllable();
+            RandomSecondSyllable();
+            RandomThirdSyllable();
+            return string.Concat(string.Concat(_firstSyllable, _secondSyllable), secondPart);
+        }
+
     }
 }
